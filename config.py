d129187c27a67395bead141e88a6be095aef7239
Muiz123 <145835848+muiz12320@users.noreply.github.com>
2024-01-28 08:18:25 +0500

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/THE-VIP-BOY-OP/VIP-MUSIC",
+    "https://github.com/muiz12320/Own2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
